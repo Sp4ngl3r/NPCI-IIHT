0c035bf31da856751559ed6027a15d49651b45ec
Sp4ngl3r <hemanth921941@gmail.com>
2021-09-17 12:07:29 +0530

    Added Assignment1

---
+++ Assignment/Assignment1.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Assignment1{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the length of array: ");
+        int n = sc.nextInt();
+
+        int a[] = new int[n];
+        System.out.println("Enter the array elements: ");
+        for (int i = 0; i < n; i++)
+            a[i] = sc.nextInt();
+
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n-i-1; j++)
+                if (a[j] > a[j+1]) {
+                    int temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
+                }
+
+        System.out.println("The ascending order of the elements are ");
+        for (int i = 0; i < n; i++) {
+                System.out.print(a[i]+" ");
+        }
+
+        System.out.println();
+        sc.close();
+    }
+}
